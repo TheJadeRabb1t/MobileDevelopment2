72f6beed3105e18b0416d59bff7424b0f21d911c
Hukyc <nukutac@gmail.com>
2023-03-11 08:01:13 +0300

    Added resources

--- app/src/main/java/com/example/ativity/MainActivity.java
+++ app/src/main/java/com/example/ativity/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.ativity;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constraint_layout);
+        TextView textView = findViewById(R.id.textView);
+        ImageView imageView = findViewById(R.id.imageView2);
+        String stringResource = getResources().getString(R.string.change_photo);
+        Bitmap imageResource = BitmapFactory.decodeResource(getResources(), R.drawable.profile_picture);
+        textView.setText(stringResource);
+        imageView.setImageBitmap(imageResource);
     }
+
+    public void changeText(View view){
+        TextView nickname = (TextView)findViewById(R.id.nickname);
+        nickname.setText("Artem Novigradov");
+        Log.e(
+                TAG, "Nickname changed"
+        );
+    }
+
 }
